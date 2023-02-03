6d82ca4aaded2e4cf63e780f7e493d0bbfb6ea05
Jay Joshi <jayrjoshi510@gmail.com>
2023-02-03 18:12:18 +0530

    db config change

--- db/config.js
+++ db/config.js
@@ -1,2 +1,3 @@
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/vdodb");
+// mongoose.connect("mongodb://localhost:27017/vdodb");
+mongoose.connect("mongodb+srv://jayjoshi88105:PzjSHKKQnuwXA24R@cluster0.096pxii.mongodb.net/test");
